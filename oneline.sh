81bd88c26bf06d6b3ad57f1bbced74e1a90ee553
Alisdair blackshaw <alisdair42@gmail.com>
2021-09-28 23:51:50 +0100

    Update oneline.sh

--- oneline.sh
+++ oneline.sh
@@ -1,6 +1,6 @@
 if [ ! -d ~/.themes]; then
   mkdir -p ~/.themes;
-mkdir ~/.themes;
+fi
 sudo pacman -Syuu git wget tar gnome-tweaks cmake jq --noconfirm;
 sudo pacman -S --needed base-devel --noconfirm;
 wget -O ~/.themes/purple.tar.xz https://dl1.pling.com/api/files/download/j/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjE2MzAzMjI4MzMiLCJ1IjpudWxsLCJsdCI6ImRvd25sb2FkIiwicyI6IjEyMjZmYmI5ZmVmYWQ2ODBiZGEzMDA4OWY5MmE5NzVjMTM2ZWIyZTFlZTUzNjdhNTY1ODNhY2M5MWJmMWM5NTUwY2VjMzc5OTZhZmU1ZDJhZjlkZWJlYjM3ZGFiM2EzNTI5NzU0ZTZmMDQ3OGY4M2E0MzZlOWZlNGI5YjI3ZjQ4IiwidCI6MTYzMjg3MTUxMCwic3RmcCI6bnVsbCwic3RpcCI6bnVsbH0.8XVOkIVFl33wQYwjeC1wuO18ZbHrGHuSujCgjYcKhm0/Orchis-purple.tar.xz;
