6ef3e949f9e323aea66251cdc08237a0d8cdaffa
Alisdair blackshaw <alisdair42@gmail.com>
2021-09-28 22:16:27 +0100

    Create oneline.sh

---
+++ oneline.sh
@@ -0,0 +1,6 @@
+mkdir ~/.themes && pacman -Syuu git wget tar gnome-tweaks --noconfirm
+wget -O ~/.themes/purple.tar.zx [get the URL]
+tar -xf ~/.themes/purple.tar.zx
+rm -f ~/.themes/purple.tar.zx
+gnome-extensions enable 'user-there@gnome-shell-extensions.gcampx.github.com'
+makepkg -si /chrome-gnome-shell
